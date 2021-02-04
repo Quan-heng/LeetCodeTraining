3965f556bd4d3f7d6e3e786900d6e83294da8f9d
heng <15364446581@163.com>
2021-02-04 08:10:46 +0800

    412

---
+++ src/main/java/Test412.java
@@ -0,0 +1,26 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test412 {
+    public static void main(String[] args) {
+        System.out.println(fizzBuzz(15));
+    }
+
+    public static List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            if((i+1)%3==0&&(i+1)%5==0){
+                result.add("FizzBuzz");
+            }else if((i+1)%5==0){
+                result.add("Buzz");
+            }else if((i+1)%3==0){
+                result.add("Fizz");
+            }else{
+                result.add(Integer.toString(i+1));
+            }
+        }
+        return result;
+    }
+}
