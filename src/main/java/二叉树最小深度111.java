50815ed1aaa454248af52e309360f02875d9d6d3
heng <15364446581@163.com>
2021-02-08 15:46:38 +0800

    二叉树的最小深度111

--- src/main/java/二叉树最小深度111.java
+++ src/main/java/二叉树最小深度111.java
@@ -14,10 +14,9 @@ public class 二叉树最小深度111 {
     public static int minDepth(TreeNode root) {
         if (root == null) {
             return 0;
-        }
-        if (root.left == null && root.right != null) {//左子树为空，计算右子树的最小深度
+        } else if (root.left == null /*&& root.right != null*/) {//左子树为空，计算右子树的最小深度
             return minDepth(root.right) + 1;
-        } else if (root.right == null && root.left != null) {//反之亦然
+        } else if (root.right == null /*&& root.left != null*/) {//反之亦然
             return minDepth(root.left) + 1;
         } else {
             return Math.min(minDepth(root.left) + 1, minDepth(root.right) + 1);//继续向下遍历，直到出现空结点
