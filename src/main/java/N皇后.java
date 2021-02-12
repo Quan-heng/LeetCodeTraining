d9d5708df47e68ec8639431ec4d5d2068063309b
quan <15364446581@163.com>
2021-02-12 12:17:02 +0800

    n皇后优化

--- src/main/java/N皇后.java
+++ src/main/java/N皇后.java
@@ -1,18 +1,78 @@
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 思路：
+ * 回溯，记录前面所有皇后能被攻击的范围
+ */
 public class N皇后 {
     public static boolean[][] used;
     public static char[][] board;
+    public static Deque<Integer> col = new LinkedList<>();
+    public static Deque<Integer> pie = new LinkedList<>();
+    public static Deque<Integer> na = new LinkedList<>();
+
     public static void main(String[] args) {
-        System.out.println(solveNQueens(10));
+        System.out.println(solveNQueens(8));
     }
+
+
+    /**
+     * 回溯，精简版，2021/02/12
+     * @param n
+     * @return
+     */
+    public static List<List<String>> solveNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        if (n <= 0) {
+            return result;
+        }
+        board = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = '.';
+            }
+        }
+        int depth = 0;
+        dfs(n, depth, result);
+        return result;
+    }
+
+    private static void dfs(int n, int depth, List<List<String>> result) {
+        if (n == depth) {
+            List<String> list = new ArrayList<>();
+            for (int i = 0; i < n; i++) {
+                list.add(new String(board[i]));
+            }
+            result.add(new ArrayList<>(list));
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (col.contains(i) || pie.contains(depth + i) || na.contains(depth - i)) {
+                continue;
+            }
+            //标记该皇后能攻击的范围（竖，撇，捺）
+            col.add(i);
+            pie.add(depth+i);
+            na.add(depth-i);
+            board[depth][i] = 'Q';
+            dfs(n, depth + 1, result);
+            //reverse，恢复上一次状态
+            board[depth][i] = '.';
+            col.removeLast();
+            pie.removeLast();
+            na.removeLast();
+        }
+    }
+    /*
+    //自己思考版，计算攻击范围有点复杂（计算竖，撇，捺即可）
     public static List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
         if(n<=0){
             return result;
         }
-        List<String> line = new ArrayList<>();
         board = new  char[n][n];
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
@@ -94,5 +154,5 @@ public class N皇后 {
             }
 
         }
-    }
+    }*/
 }
--- src/main/java/Test17.java
+++ src/main/java/Test17.java
@@ -6,10 +6,9 @@ import java.util.*;
  */
 public class Test17 {
     public static void main(String[] args) {
-        generateMap();
-        System.out.println(letterCombinations("234"));
+        System.out.println(letterCombinations("23"));
     }
-    public static Map<Integer, String> phoneMap = new HashMap<>();
+    public static Map<Character, String> phoneMap = new HashMap<>();
     private static void generateMap() {
         /*char begin = 'a';
         int end;
@@ -25,15 +24,41 @@ public class Test17 {
             }
             phoneMap.put(i, chars);
         }*/
-        phoneMap.put(2,"abc");
-        phoneMap.put(3,"def");
-        phoneMap.put(4,"ghi");
-        phoneMap.put(5,"jkl");
-        phoneMap.put(6,"mno");
-        phoneMap.put(7,"pqrs");
-        phoneMap.put(8,"tuv");
-        phoneMap.put(9,"wxyz");
+        phoneMap.put('2',"abc");
+        phoneMap.put('3',"def");
+        phoneMap.put('4',"ghi");
+        phoneMap.put('5',"jkl");
+        phoneMap.put('6',"mno");
+        phoneMap.put('7',"pqrs");
+        phoneMap.put('8',"tuv");
+        phoneMap.put('9',"wxyz");
     }
+
+    /**
+     * 视频版,效率没有第一版高
+     * @param digits
+     * @return
+     */
+    /*public static List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if(digits.isEmpty()){
+            return result;
+        }
+        generateMap();
+        dfs(digits,"",result,0);
+        return result;
+    }
+
+    private static void dfs(String digits, String letters, List<String> result, int depth) {
+        if(digits.length()==depth){
+            result.add(letters);
+            return;
+        }
+        String value= phoneMap.get(digits.charAt(depth));
+        for (int i = 0; i < value.length(); i++) {
+            dfs(digits,letters+value.charAt(i),result,depth+1);
+        }
+    }*/
 
     public static List<String> letterCombinations(String digits) {
         List<String> result = new ArrayList<>();
@@ -43,25 +68,22 @@ public class Test17 {
         generateMap();
         StringBuilder combineStr = new StringBuilder();
         int depth = 0;
-        int[] numberList = new int[digits.length()];
-        for(int i=0;i<digits.length();i++){
-            numberList[i]=digits.charAt(i)-'0';
-        }
-        dfs(numberList,combineStr,result,depth,digits.length());
+        dfs(digits,combineStr,result,depth,digits.length());
         return result;
     }
 
-    private static void dfs(int[] numberList, StringBuilder combineStr, List<String> result, int depth,int max) {
+    private static void dfs(String digits, StringBuilder combineStr, List<String> result, int depth,int max) {
         if(max==depth){
             result.add(combineStr.toString());
             return;
         }
-        String value= phoneMap.get(numberList[depth]);
+        String value= phoneMap.get(digits.charAt(depth));
         for (int i = 0; i < value.length(); i++) {
             combineStr.append(value.charAt(i));
-            dfs(numberList,combineStr,result,depth+1,max);
+            dfs(digits,combineStr,result,depth+1,max);
             //reverse
             combineStr.deleteCharAt(combineStr.length()-1);
         }
     }
+
 }
