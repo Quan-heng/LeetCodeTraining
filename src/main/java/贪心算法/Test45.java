0b6c21af2d690adf81b1f85c68cb9a79166e4c56
quanheng <15364446581@163.com>
2021-02-21 21:09:39 +0800

    贪心算法，跳跃II

---
+++ src/main/java/贪心算法/Test45.java
@@ -0,0 +1,21 @@
+package 贪心算法;
+
+/**
+ * 思路：贪心算法,
+ * 从前往后跳，记录能跳跃的最大位置，记录下一步能跳跃的最大距离
+ */
+public class Test45 {
+    public int jump(int[] nums) {
+        int end = 0;
+        int maxPosition = 0;
+        int step=0;
+        for (int i = 0; i < nums.length-1; i++) {//最后一个下标不需要访问
+            maxPosition  = Math.max(nums[i]+i,maxPosition);
+            if(i==end){
+                end = maxPosition;
+                step++;
+            }
+        }
+        return step;
+    }
+}
