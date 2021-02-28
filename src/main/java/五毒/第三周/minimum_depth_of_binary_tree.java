3c213b1363821ba755be1e12b829057853dfcf93
quanheng <15364446581@163.com>
2021-02-28 18:31:21 +0800

    五毒第三周

---
+++ src/main/java/五毒/第三周/minimum_depth_of_binary_tree.java
@@ -0,0 +1,19 @@
+package 五毒.第三周;
+
+/**
+ * 思路：递归
+ */
+public class minimum_depth_of_binary_tree {
+    public int minDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        if(root.left==null){
+            return minDepth(root.right);
+        }
+        if(root.right==null){
+            return minDepth(root.left);
+        }
+       return Math.min(minDepth(root.left),minDepth(root.right))+1;
+    }
+}
