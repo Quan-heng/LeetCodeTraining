661b16db8f086be0011eafdc1ad58fe83a653e68
quanheng <15364446581@163.com>
2021-02-28 22:02:55 +0800

    五毒第三周

---
+++ src/main/java/五毒/第三周/serialize_and_deseriaize_binary_tree.java
@@ -0,0 +1,100 @@
+package 五毒.第三周;
+
+import java.util.*;
+
+public class serialize_and_deseriaize_binary_tree {
+    // Encodes a tree to a single string.
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3= new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+        node1.left = node2;
+        node1.right = node3;
+        node3.left = node4;
+        node3.right = node5;
+        System.out.println(deserialize(serialize(node1)));
+
+    }
+    public static String serialize(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root==null){
+           list.toString();
+        }
+        Deque<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean isEnd;
+        while (!queue.isEmpty()){
+            isEnd = true;
+            int size = queue.size();
+            List<Integer> subList = new ArrayList<>();
+            for(int i=0;i<size;i++){
+                TreeNode node = queue.removeFirst();
+                if(node!=null){
+                    subList.add(node.val);
+                    queue.add(node.left);
+                    queue.add(node.right);
+                    isEnd = false;
+                }else{
+                    subList.add(null);
+                }
+            }
+            if(!isEnd){
+                list.addAll(subList);
+            }
+        }
+        return list.toString();
+
+    }
+
+    // Decodes your encoded data to tree.
+    public static TreeNode deserialize(String data) {
+        String str = data.substring(1,data.length()-1).replace(" ","");
+        if(str.isEmpty()){
+            return null;
+        }
+        String[] array = str.split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(array[0]));
+        TreeNode result = root;
+        int count=1;
+        int index=1;
+        Deque<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+        while(!queue.isEmpty()&&count<array.length){
+            int size = queue.size();
+            int curCount = (int)Math.pow(2,index);
+            int temp= 0;
+            for(int i=0;i<size;i++){
+                root = queue.removeFirst();
+                TreeNode left;
+                TreeNode right;
+                if(array[count].equals("null")){
+                    left = null;
+                }else{
+                    left = new TreeNode(Integer.valueOf(array[count]));
+                }
+                count++;
+
+                if(array[count].equals("null")){
+                    right = null;
+                }else{
+                    right= new TreeNode(Integer.valueOf(array[count]));
+                }
+                count++;
+
+                root.left=left;
+                root.right = right;
+                queue.add(left);
+                queue.add(right);
+                temp+=2;
+                if(temp==curCount){
+                    break;
+                }
+            }
+            index++;
+        }
+        return result;
+    }
+}
