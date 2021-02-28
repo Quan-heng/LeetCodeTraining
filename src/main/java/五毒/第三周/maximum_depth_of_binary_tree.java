3ff8e04117440ef9e4458a0b793de8f6dc9e3bec
quanheng <15364446581@163.com>
2021-02-28 18:15:04 +0800

    五毒第三周

---
+++ src/main/java/五毒/第三周/maximum_depth_of_binary_tree.java
@@ -0,0 +1,18 @@
+package 五毒.第三周;
+
+/**
+ * 思路：递归
+ * 时间复杂度: o(n),n为所有元素的数量
+ * 空间复杂度: o(height),height为树的高度
+ */
+
+public class maximum_depth_of_binary_tree {
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int left = maxDepth(root.left)+1;
+        int right = maxDepth(root.right)+1;
+        return Math.max(left,right);
+    }
+}
