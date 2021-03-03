341e0b2aa216e1674243103dc9c9412a24bb7655
heng <15364446581@163.com>
2021-03-03 09:12:46 +0800

    每日一题

--- src/main/java/五毒/第一周/第一遍/rotate_array.java
+++ src/main/java/五毒/第一周/第一遍/rotate_array.java
@@ -1,16 +1,23 @@
 package 五毒.第一周.第一遍;
 
 public class rotate_array {
-    public void rotate(int[] nums, int k) {
-        int n = 0;
-        int index = 0;
-        int cur = nums[0];
-        while (n <= nums.length) {
-            int temp = nums[(index + k) % nums.length];
-            nums[(index + k) % nums.length] = cur;
-            cur = temp;
-            index=(index + k) % nums.length;
-            n++;
+    public static void main(String[] args) {
+        int[] nums = new int[]{1,2,3,4,5,6,7};
+        rotate(nums,3);
+    }
+    public static void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k-1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        int temp;
+        while (start < end) {
+            temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
         }
     }
 }
