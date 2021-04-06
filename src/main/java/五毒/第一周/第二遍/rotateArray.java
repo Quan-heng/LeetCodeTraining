36aa1e08e5b51ae6c36670b75af9022f432ad7a9
heng <15364446581@163.com>
2021-04-06 16:53:01 +0800

    largest rectangle

---
+++ src/main/java/五毒/第一周/第二遍/rotateArray.java
@@ -0,0 +1,18 @@
+package 五毒.第一周.第二遍;
+
+public class rotateArray {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        for (int i = start, j = end; i < j; i++, j--) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
