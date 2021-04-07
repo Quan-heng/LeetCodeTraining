60630841a25db6d530f434fc99a338d9e4ef885c
heng <15364446581@163.com>
2021-04-07 14:53:27 +0800

    Merge branch 'master' of https://github.com/Quan-heng/LeetCodeTraining

---
+++ src/main/java/五毒/第二周/第二遍/TreeNode.java
@@ -0,0 +1,20 @@
+package 五毒.第二周.第二遍;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
