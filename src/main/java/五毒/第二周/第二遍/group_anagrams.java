fdd50544636e71c22ef12a606142a4ba66d4f7aa
heng <15364446581@163.com>
2021-04-07 13:43:26 +0800

    Merge branch 'master' of https://github.com/Quan-heng/LeetCodeTraining

---
+++ src/main/java/五毒/第二周/第二遍/group_anagrams.java
@@ -0,0 +1,22 @@
+package 五毒.第二周.第二遍;
+
+import java.util.*;
+
+public class group_anagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            char[] charArray = strs[i].toCharArray();
+            Arrays.sort(charArray);
+            String sortedStr = String.valueOf(charArray);
+            List<String> list = map.getOrDefault(sortedStr,new ArrayList<>());
+            list.add(strs[i]);
+            map.put(sortedStr,list);
+        }
+        List<List<String>> result = new ArrayList<>();
+        map.values().forEach(a->{
+            result.add(a);
+        });
+        return result;
+    }
+}
