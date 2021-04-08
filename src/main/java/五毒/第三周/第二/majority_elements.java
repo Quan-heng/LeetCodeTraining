4a1bd7ec1e136ec58f22d9afbe70264a2eea2ba4
heng <15364446581@163.com>
2021-04-08 16:42:35 +0800

    Merge branch 'master' of https://github.com/Quan-heng/LeetCodeTraining
    # Conflicts:
    #	算法24期-五毒神掌练习.xlsx

---
+++ src/main/java/五毒/第三周/第二/majority_elements.java
@@ -0,0 +1,32 @@
+package 五毒.第三周.第二;
+
+import java.util.Arrays;
+
+public class majority_elements {
+    /**
+     * 摩尔投票法
+     * @param nums
+     * @return
+     */
+    public int majorityElement(int[] nums) {
+        int candidate= nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if(candidate==nums[i]){
+                count++;
+            }else{
+                if(--count==0){
+                    candidate  = nums[i];
+                    count=1;
+                }
+            }
+        }
+        return candidate;
+    }
+
+    /*
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }*/
+}
