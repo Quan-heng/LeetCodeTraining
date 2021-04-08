b69e724e1505714ec4bf6faa3ca4943d8e78c24c
heng <15364446581@163.com>
2021-04-08 16:32:10 +0800

    Merge branch 'master' of https://github.com/Quan-heng/LeetCodeTraining
    # Conflicts:
    #	算法24期-五毒神掌练习.xlsx

---
+++ src/main/java/五毒/第三周/第二/subsets.java
@@ -0,0 +1,28 @@
+package 五毒.第三周.第二;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        dfs(result,new ArrayList<>(),nums,0);
+        return result;
+    }
+
+    private void dfs(List<List<Integer>> result, List<Integer> subList, int[] nums, int depth) {
+        if(depth==nums.length){
+            result.add(new ArrayList<>(subList));
+            return;
+        }
+
+        dfs(result,subList,nums,depth+1);
+        subList.add(nums[depth]);
+        dfs(result,subList,nums,depth+1);
+        subList.remove(subList.size()-1);
+
+    }
+
+
+}
