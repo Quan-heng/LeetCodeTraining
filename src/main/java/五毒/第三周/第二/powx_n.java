9c8f3e37d4eec08f6d6a71cad222c046782b487f
heng <15364446581@163.com>
2021-04-08 16:15:38 +0800

    Merge branch 'master' of https://github.com/Quan-heng/LeetCodeTraining
    # Conflicts:
    #	算法24期-五毒神掌练习.xlsx

---
+++ src/main/java/五毒/第三周/第二/powx_n.java
@@ -0,0 +1,21 @@
+package 五毒.第三周.第二;
+
+public class powx_n {
+    public static double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == -1) {
+            return 1 / x;
+        }
+        if (n == 1) {
+            return x;
+        }
+        double half = myPow(x, n / 2);
+        return half * half * myPow(x, n % 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myPow(2, 10));
+    }
+}
