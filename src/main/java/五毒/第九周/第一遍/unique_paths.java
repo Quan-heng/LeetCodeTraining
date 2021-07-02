eb990d5083d25d4a706e98f0c087b47a03d3c002
quan <heng.quan@jaka.com>
2021-07-02 10:04:19 +0800

    第九周-动态规划

---
+++ src/main/java/五毒/第九周/第一遍/unique_paths.java
@@ -0,0 +1,29 @@
+package 五毒.第九周.第一遍;
+
+public class unique_paths {
+    /**
+     * 状态转移方程
+     * dp[m][n] = dp[m-1][n] + dp[m][n-1]
+     *
+     * @param m
+     * @param n
+     * @return
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        return dp[m - 1][n - 1];
+    }
+}
